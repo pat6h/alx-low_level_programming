8258d3c3ce50a45a653583b053fb13940109bfdc
Ayoub <ayoubzerou@gmail.com>
2024-06-20 16:14:02 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - prints exactly with proper grammar, but the outcome is a piece of art,
+ * Return: 0 (success)
+ */
+int main(void){
+	printf("\"with proper grammar, but the outcome is a piece of art,")
+		return (0);
+}

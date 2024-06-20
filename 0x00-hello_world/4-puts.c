8cf61cd23720b839abcbb38de9545151d8e4403e
Ayoub <ayoubzerou@gmail.com>
2024-06-20 15:54:18 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,12 @@
 
 /**
  * main - prints the phrase Programming is like building a multilingual puzzle
- * Return: 0 if exited properly, non-zero otherwise
+ * Return: 0 (Success)
  */
-int main() {
-  puts("Programming is like building a multilingual puzzle\n");
+
+int main() 
+{
+  puts("\"Programming is like building a multilingual puzzle");
   return 0;
 
 }

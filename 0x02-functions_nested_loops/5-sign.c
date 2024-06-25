d933fb603ccf64b8e35ebc0e93450cab0b6d80d3
Ayoub <ayoubzerou@gmail.com>
2024-06-25 06:24:48 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,15 +6,17 @@
 */
 int print_sign(int n)
 {
-if (n > 0)
-{
-putchar('+');
-return 1;
-} else if (n < 0) {
-putchar('-');
-return -1;
-} else {
-putchar('0');
-return 0;
-}
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');
+	}
+		return (-1);
 }

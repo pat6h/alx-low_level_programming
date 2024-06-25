feb09a6d169dfd371713107ae800d8960541b4bb
Ayoub <ayoubzerou@gmail.com>
2024-06-25 06:08:29 -0700

    task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+*_islower -  in lowercase
+*@c: the character to check
+*Return:0
+*/
+int _isalpha(int c)
+{
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}

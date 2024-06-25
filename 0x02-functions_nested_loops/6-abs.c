964a216d7d66c740a6f41c8d0227ae8d899538cd
Ayoub <ayoubzerou@gmail.com>
2024-06-25 07:49:10 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*print_sign -  prints the sign of a number
+*@n : to check
+*Return:0
+*/
+int _abs(int n)
+{
+  if (n < 0) {
+    return -n;
+  } else {
+    return n;
+  }
+}

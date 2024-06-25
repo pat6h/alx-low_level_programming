a370cbefb5411004f6ed8480f2a40d74363b2dd7
Ayoub <ayoubzerou@gmail.com>
2024-06-25 04:43:44 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,13 +3,14 @@
  * main - program that prints _putchar,
  * Return: Always 0 (Success)
  */
-int main(){
-  char name[] = "_putchar\n";
-    int a = 0;
-  do
-  {
-    putchar(name[a]);
-    a++;
-  } while (name[a] != '\0');
-  return 0;
+int main(void)
+{
+char name[] = "_putchar\n";
+int a = 0;
+do
+{
+putchar(name[a]);
+a++;
+} while (name[a] != '\0');
+return 0;
 }

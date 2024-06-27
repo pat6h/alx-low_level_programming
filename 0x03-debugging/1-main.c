542ce98f2f7af6d35a2a7264d0d66afd66e38bf9
Ayoub <ayoubzerou@gmail.com>
2024-06-26 18:38:15 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,11 +13,11 @@ printf("Infinite loop incoming :(\n");
 i = 0;
 
 /**
+* main - Commenting out the while loop prevents the infinite loop
 * while (i < 10) {
 * putchar(i);
 * }
 */
-
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);

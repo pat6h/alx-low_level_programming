6603c21dfb61a0e712a7edf574fb40995b9d4856
Ayoub <ayoubzerou@gmail.com>
2024-07-10 15:46:40 -0700

    task end

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -15,7 +15,7 @@ if (*s2 == '*')
 {
 if (*s1 == '\0' && *(s2 + 1) != '\0')
 return (0);
-return (wildcmp(s1, s2 + 1) || ( *s1 != '\0' && wildcmp(s1 + 1, s2)));
+return (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)));
 }
 else if (*s1 != '\0' && *s1 == *s2)
 return (wildcmp(s1 + 1, s2 + 1));

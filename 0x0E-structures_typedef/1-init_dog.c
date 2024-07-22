507cbc220e3ab0c51ea141baa15c9ac434a0259a
Ayoub <ayoubzerou@gmail.com>
2024-07-22 16:34:23 -0700

    all

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,29 +1,20 @@
-#include <dog.h>
-struct dog
-{
-char name[50];
-float age;
-char owner[50];
-};
+#include "dog.h"
+
 /**
- * init_dog - Initializes a variable of type struct dog
- * @d: Pointer to the struct dog variable to be initialized
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Name of the dog's owner
- */
+* init_dog - inits a variable of
+*type dog
+*@d: dog identification
+*@name: name of dog
+*@age: age of dog
+*@owner: owner's name
+*/
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-strncpy(d->name, name, sizeof(d->name) - 1);
-d->name[sizeof(d->name) - 1] = '\0';
-d->age = age;
-strncpy(d->owner, owner, sizeof(d->owner) - 1);
-d->owner[sizeof(d->owner) - 1] = '\0';
-}
-int main(void)
+if (d != NULL)
 {
-struct dog my_dog;
-init_dog(&my_dog, "Poppy", 3.5, "Bob");
-printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-return (0);
+(*d).name = name;
+(*d).age = age;
+(*d).owner = owner;
+}
 }
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,35 @@
-#ifndef DOG_H_
-#define DOG_H_
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * struct dog - A structure to represent a dog
- * @name: The dog's name (char pointer)
- * @age: The dog's age (float)
- * @owner: The dog's owner's name (char pointer)
- */
-typedef struct dog
+*struct dog - a new type of data
+*representing a dog
+*@name: name of dog
+*@age: age of dog
+*@owner: owner's name
+*/
+struct dog
 {
 char *name;
 float age;
 char *owner;
-}
-dog_t;
+};
+
+
+/**
+*dog_t - Typedef for struct dog
+*/
+typedef struct dog dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *str);
+char *_strcopy(char *dest, char *src);
 
 #endif

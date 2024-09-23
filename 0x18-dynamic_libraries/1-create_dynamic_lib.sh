a7301c88fe20a93e7c638dd27b9f19a651d256e1
Ayoub <ayoubzerou@gmail.com>
2024-09-23 14:28:03 -0700

    tasks

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
+# Compile all .c files into position-independent code and create a shared library
+
+# Compile the .c files into object files
 gcc -c -fPIC *.c
+
+# Create the dynamic library from the object files
 gcc -shared -o liball.so *.o
+
+# Clean up the object files
+rm *.o
+

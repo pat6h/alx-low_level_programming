37342a3ea158503839f5d963fdeb4a4c115e4bad
Ayoub <ayoubzerou@gmail.com>
2024-11-27 15:07:45 -0800

    all

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,40 +1,22 @@
 #!/usr/bin/python3
-"""
-Module 5-island_perimeter
-Defines the island_perimeter function.
-"""
+"""5-island_perimeter module"""
+
 
 def island_perimeter(grid):
-    """
-    Returns the perimeter of the island described in grid.
-    
-    Args:
-        grid (list of list of ints): A rectangular grid where:
-            - 0 represents water
-            - 1 represents land
-            - Grid cells are connected horizontally/vertically (not diagonally)
-    
-    Returns:
-        int: The perimeter of the island.
-    """
-    perimeter = 0
-    rows = len(grid)
-    cols = len(grid[0]) if rows > 0 else 0
+    """Returns the perimeter of the island described in grid"""
+    c = 0
+    length = len(grid) - 1
+    width = len(grid[0]) - 1
 
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:  # Land cell
-                # Check top
-                if i == 0 or grid[i - 1][j] == 0:
-                    perimeter += 1
-                # Check bottom
-                if i == rows - 1 or grid[i + 1][j] == 0:
-                    perimeter += 1
-                # Check left
-                if j == 0 or grid[i][j - 1] == 0:
-                    perimeter += 1
-                # Check right
-                if j == cols - 1 or grid[i][j + 1] == 0:
-                    perimeter += 1
-    return perimeter
-
+    for i, r in enumerate(grid):
+        for j, n in enumerate(r):
+            if n == 1:
+                if i == 0 or grid[i - 1][j] != 1:
+                    c += 1
+                if j == 0 or grid[i][j - 1] != 1:
+                    c += 1
+                if j == width or grid[i][j + 1] != 1:
+                    c += 1
+                if i == length or grid[i + 1][j] != 1:
+                    c += 1
+    return c
